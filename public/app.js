a1fd4dacab2b0371a88c44439089b9d2b33cfe1a
nigahiga34 <kingoc39@gmail.com>
2024-08-26 09:38:03 +0100

    qwertyuioplkjhgf

--- public/app.js
+++ public/app.js
@@ -133,3 +133,105 @@ socket.on('incoming call', ({ withVideo }) => {
         startCall(withVideo);
     }
 });
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+const rooms = new Map();
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  socket.on('join', (roomName) => {
+    socket.join(roomName);
+    if (!rooms.has(roomName)) {
+      rooms.set(roomName, new Set());
+    }
+    rooms.get(roomName).add(socket.id);
+    socket.to(roomName).emit('user-joined', socket.id);
+    console.log(`User ${socket.id} joined room ${roomName}`);
+  });
+
+  socket.on('leave', (roomName) => {
+    socket.leave(roomName);
+    if (rooms.has(roomName)) {
+      rooms.get(roomName).delete(socket.id);
+      if (rooms.get(roomName).size === 0) {
+        rooms.delete(roomName);
+      }
+    }
+    socket.to(roomName).emit('user-left', socket.id);
+    console.log(`User ${socket.id} left room ${roomName}`);
+  });
+
+  // WebRTC signaling
+  socket.on('offer', ({ offer, to }) => {
+    socket.to(to).emit('offer', { offer, from: socket.id });
+  });
+
+  socket.on('answer', ({ answer, to }) => {
+    socket.to(to).emit('answer', { answer, from: socket.id });
+  });
+
+  socket.on('ice-candidate', ({ candidate, to }) => {
+    socket.to(to).emit('ice-candidate', { candidate, from: socket.id });
+  });
+
+  // Text chat
+  socket.on('message', ({ room, message }) => {
+    socket.to(room).emit('message', { sender: socket.id, message });
+  });
+
+  // Audio stream started
+  socket.on('audio-start', (roomName) => {
+    socket.to(roomName).emit('user-audio-start', socket.id);
+    console.log(`User ${socket.id} started audio in room ${roomName}`);
+  });
+
+  // Audio stream stopped
+  socket.on('audio-stop', (roomName) => {
+    socket.to(roomName).emit('user-audio-stop', socket.id);
+    console.log(`User ${socket.id} stopped audio in room ${roomName}`);
+  });
+
+  // Audio volume change
+  socket.on('audio-volume-change', ({ roomName, volume }) => {
+    socket.to(roomName).emit('user-audio-volume-change', { userId: socket.id, volume });
+  });
+
+  // Mute/unmute
+  socket.on('toggle-mute', ({ roomName, isMuted }) => {
+    socket.to(roomName).emit('user-mute-change', { userId: socket.id, isMuted });
+    console.log(`User ${socket.id} ${isMuted ? 'muted' : 'unmuted'} in room ${roomName}`);
+  });
+
+  socket.on('disconnecting', () => {
+    for (const room of socket.rooms) {
+      if (rooms.has(room)) {
+        rooms.get(room).delete(socket.id);
+        if (rooms.get(room).size === 0) {
+          rooms.delete(room);
+        }
+        socket.to(room).emit('user-left', socket.id);
+      }
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
